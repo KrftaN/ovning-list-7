5b8996110f437c0162320bbe31d3361c33ca4b41
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 14:22:45 +0200

    Add project files.

---
+++ ovning list 7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace övning_list._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "a", "b", "b", "c", "hej" };
+            string[] array2 = { "hej", "a", "a", "b", "programmering" };
+
+            string[] gemensammaSträngar = FinnsIBåda(array1, array2);
+
+            Console.WriteLine("Gemensamma strängar:");
+            foreach (string sträng in gemensammaSträngar)
+            {
+                Console.WriteLine(sträng);
+            }
+        }
+
+        static string[] FinnsIBåda(string[] array1, string[] array2)
+        {
+            var gemensamma = array1.Intersect(array2).Distinct().ToArray();
+            return gemensamma;
+        }
+    }
+}
